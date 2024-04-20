79040e43d02c92179910295d998c92fc22c2e42b
CgtzNoemi <89617943+CgtzNoemi@users.noreply.github.com>
2024-04-19 21:28:21 -0600

    Update salarios.component.ts

--- src/app/salarios/salarios.component.ts
+++ src/app/salarios/salarios.component.ts
@@ -85,8 +85,9 @@ export class SalariosComponent {
       if (!isNaN(salarioBase) && !isNaN(bonificaciones) && !isNaN(comisiones)) {
         const salarioBruto = salarioBase + bonificaciones;
         let salarioNeto = salarioBruto - comisiones;
-        const impuestos = salarioBruto * 0.03; 
-        salarioNeto -= impuestos; 
+        const impuestos = salarioBruto * 0.03; // Impuestos sobre el salario bruto
+        salarioNeto -= impuestos; // Restar impuestos al salario neto
+        console.log(salarioNeto);
         this.salarioNetoPorEmpleado[id] = salarioNeto;
         return salarioNeto;
       }
